121e19e3b28f54ebcfc33a51fe8f66a339306b9a
Alex Hamon <alex.hmn18@gmail.com>
2024-04-04 17:35:50 +0200

    card finis

--- index.php
+++ index.php
@@ -10,45 +10,70 @@ require_once  'layout/nav.php';
 <section class="py-5">
     <div class="container">
         <h2 class="text-center mb-4">Calendar</h2>
-        <div class="row">
+        <div class="row p-4">
 
-            <div class="col-md-3 card p-0">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuxbBE6vMO6wXK6FLLbvT-ax9JU68XInMhaw&usqp=CAU" class="card-img-top w-100 img-fluid" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="col-lg-4 col-md-6 col-12">
+                <div class="mb-4">
+                    <div class="card text-center">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuxbBE6vMO6wXK6FLLbvT-ax9JU68XInMhaw&usqp=CAU" 
+                            class="card-img-top img-fluid" alt="...">
+                        <div class="card-body">
+                            <div class="card-header">
+                                <h4 class="card-title">Match n°</h4>
+                                <div class="d-flex justify-content-evenly">
+                                    <div class="d-flex">
+                                        <img src="" alt="">
+                                        <h5 class="card-title">TeamDomicile : </h5>
+                                    </div>
+                                    <h5 class="card-title">4</h5>
+                                </div>
+                                <div class="d-flex justify-content-evenly">
+                                    <div class="d-flex">
+                                        <img src="" alt="">
+                                        <h5 class="card-title">TeamExtérieur : </h5>
+                                    </div>
+                                    <h5 class="card-title">2</h5>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Date et heure du match</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">See more</a>
+                                <a href="#" class="btn btn-primary">Modify</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
 
 
-            <!-- Card à dupliquer -->
-            <!-- <div class="col-md-4 col-6 mb-4">
-                <div class="card text-center " style="background-color:#ffffff;">
-                    <div class="bg-image hover-overlay ripple">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuxbBE6vMO6wXK6FLLbvT-ax9JU68XInMhaw&usqp=CAU" class="img-fluid w-100" />
-                        <a href="#">
-                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                        </a>
-                    </div>
-                    <div class="card-header">Equipe 1 : 0 - 0 : Equipe 2</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Match n°x</h5>
-                        <p class="card-text">
-                            Date et heure début
-                        </p>
-                    </div>
-                    <div class="card-footer">Lieu et heure du RDV</div>
+
+
+
+
+
+
+
+
+
+
+            <div class="col-lg-4 col-md-6 col-12">
+                <div class="mb-4 border h-100">
+
+                    <p>test</p>
+
                 </div>
-            </div> -->
-            
+            </div>
 
         </div>
     </div>
 </section>
+
+
     
 
 
 
-<?php require_once 'layout/footer.php';
+<?php require_once 'layout/footer.php';?>
